f8c31c2811c788b74f6abfb7e78c3c57599eff3f
alfian130 <alfian.130656@mhs.amikomsolo.ac.id>
2026-05-25 21:21:53 +0700

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,36 +1,36 @@
 import streamlit as st
 
-st.title("Sistem Pakar Diagnosa Penyakit")
+st.title("Sistem Pakar Diagnosa Penyakit Tanaman")
 
-st.write("Pilih gejala yang dialami:")
+st.write("Pilih gejala pada tanaman:")
 
 # Input gejala
-demam = st.checkbox("Demam")
-batuk = st.checkbox("Batuk")
-sakit_tenggorokan = st.checkbox("Sakit Tenggorokan")
-sesak_napas = st.checkbox("Sesak Napas")
-hidung_tersumbat = st.checkbox("Hidung Tersumbat")
-sakit_kepala = st.checkbox("Sakit Kepala")
-mual = st.checkbox("Mual")
-diare = st.checkbox("Diare")
+daun_menguning = st.checkbox("Daun Menguning")
+bercak_coklat = st.checkbox("Daun Bercak Coklat")
+batang_busuk = st.checkbox("Batang Busuk")
+tanaman_layu = st.checkbox("Tanaman Layu")
+daun_berlubang = st.checkbox("Daun Berlubang")
+pertumbuhan_lambat = st.checkbox("Pertumbuhan Lambat")
+akar_membusuk = st.checkbox("Akar Membusuk")
+daun_menggulung = st.checkbox("Daun Menggulung")
 
-diagnosa = "Belum ditemukan penyakit yang sesuai"
+diagnosa = "Penyakit belum terdeteksi"
 
 # Rule-based system
-if demam and batuk and sakit_tenggorokan:
-    diagnosa = "Flu"
+if daun_menguning and pertumbuhan_lambat:
+    diagnosa = "Tanaman Kekurangan Nutrisi"
 
-elif demam and sesak_napas and batuk:
-    diagnosa = "COVID-19"
+elif bercak_coklat and daun_menggulung:
+    diagnosa = "Tanaman Terkena Jamur"
 
-elif hidung_tersumbat and sakit_tenggorokan:
-    diagnosa = "Common Cold"
+elif batang_busuk and akar_membusuk:
+    diagnosa = "Tanaman Terkena Busuk Akar"
 
-elif mual and diare and sakit_kepala:
-    diagnosa = "Keracunan Makanan"
+elif daun_berlubang:
+    diagnosa = "Tanaman Terkena Hama Ulat"
 
-elif demam and sakit_kepala and mual:
-    diagnosa = "Demam Berdarah"
+elif tanaman_layu and daun_menguning:
+    diagnosa = "Tanaman Kekurangan Air"
 
 # Tombol diagnosa
 if st.button("Diagnosa"):
